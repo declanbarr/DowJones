4d8a8d96fa45721f6db2739b77f13ebb406dc117
Declan Barr <35577478+declanbarr@users.noreply.github.com>
2018-07-24 21:24:07 +0100

    Added comments

--- DowAna.py
+++ DowAna.py
@@ -1,4 +1,9 @@
+# Declan Barr 18 July 2018
+# Script to analyse Dow Jones Index Data Set from the UCI Machine Learning Repository
+
 import pandas as pd 
 
 df = pd.read_csv('dow_jones_index.data')
 print(df)
+
+
