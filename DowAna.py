a7d87c54e1948bb9fdce52d1ad91f9356609799c
Declan Barr <35577478+declanbarr@users.noreply.github.com>
2018-07-24 21:34:43 +0100

    Set max_rows to 999 and max_columns to 100

--- DowAna.py
+++ DowAna.py
@@ -3,6 +3,9 @@
 
 import pandas as pd 
 
+pd.options.display.max_rows = 999 # Set max rows to 999 to ensure all rows are displayed (from https://pandas.pydata.org/pandas-docs/stable/options.html)
+pd.options.display.max_columns = 100 # Set max columns to 100 to ensure all data is shown for describe() function (from https://pandas.pydata.org/pandas-docs/stable/options.html)
+
 df = pd.read_csv('dow_jones_index.data')
 print(df)
 
