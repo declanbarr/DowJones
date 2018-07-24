2561dc73f7c68550aae4fe25421ac0506d949f3a
Declan Barr <deckybarr@googlemail.com>
2018-07-24 22:25:59 +0100

    df.iloc for column 3 not showing full describe output

--- DowAna.py
+++ DowAna.py
@@ -10,4 +10,4 @@ pd.options.display.max_columns = 100 # Set max columns to 100 to ensure all data
 df = pd.read_csv('dow_jones_index.data')
 #print(df.head(10))
 
-print(df.describe())
+print(df.iloc[:, [3]].describe())
