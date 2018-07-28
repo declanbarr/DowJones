e611f7f7f53b437fb0f3ff203478f1b2f7f4423d
Declan Barr <deckybarr@googlemail.com>
2018-07-28 21:45:58 +0100

    pd.read_csv updated to convert OHLC to values without $.

--- DowAna.py
+++ DowAna.py
@@ -6,8 +6,7 @@ import pandas as pd
 pd.options.display.max_rows = 999 # Set max rows to 999 to ensure all rows are displayed (from https://pandas.pydata.org/pandas-docs/stable/options.html)
 pd.options.display.max_columns = 100 # Set max columns to 100 to ensure all data is shown for describe() function (from https://pandas.pydata.org/pandas-docs/stable/options.html)
 
-
-df = pd.read_csv('dow_jones_index.data', converters={'open': lambda s: float(s.replace('$', ''))}) # From https://stackoverflow.com/questions/36320317/pandas-read-csv-ignore-dollar-sign-when-parsing-numbers
+df = pd.read_csv('dow_jones_index.data', converters={'open': lambda s: float(s.replace('$', '')), 'close': lambda s: float(s.replace('$', '')), 'high': lambda s: float(s.replace('$', '')), 'low': lambda s: float(s.replace('$', ''))}) # From https://stackoverflow.com/questions/36320317/pandas-read-csv-ignore-dollar-sign-when-parsing-numbers
 #print(df.head(10))
-
-print(df.iloc[:, [3]].describe())
+for x in range(3,7):
+    print(df.iloc[:, [x]].describe())
